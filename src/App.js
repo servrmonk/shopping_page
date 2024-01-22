03eb707b9b07910e03d4ce97b6451bb5fed869df
Raushan <roushankumarsingh2018@gmail.com>
2024-01-22 13:34:23 +0530

    cart working dynamically

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { Provider, useSelector } from 'react-redux';
+import {  useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import store from './store/index';
+
 
 
 function App() {
   const showCart  = useSelector((state) => state.ui.cartIsVisible)
   return (
-    <Provider store={store}>
+    
     <Layout>
       {showCart && <Cart />}
       <Products />
     </Layout>
-    </Provider>
+    
   );
 }
 
